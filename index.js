a866d672da01548e37c046673902e2a713208123
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-17 23:35:42 -0300

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const DataBase = require('./src/dataBase/db.js')
 require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.use(cors())
 app.use(express.static('public'))
